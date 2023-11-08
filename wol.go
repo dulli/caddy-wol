b4aa30181ab823bb631ededbf8b0c08ed5008916
Michael Ellis <michael94ellis@gmail.com>
2023-11-08 16:21:13 -0500

    refactor: pre-allocate packet memory (#15)

--- wol.go
+++ wol.go
@@ -4,13 +4,15 @@ import "net"
 
 func BuildMagicPacket(mac net.HardwareAddr) []byte {
 	macBytes := []byte(mac)
-	mp := make([]byte, 6)
+	// Pre-allocate packet memory
+	packetLen := 6 + 16*len(macBytes)
+	mp := make([]byte, packetLen)
 	for idx := 0; idx < 6; idx++ {
 		mp[idx] = 0xFF
 	}
-	for idx := 0; idx < 16; idx++ {
-		// TODO: refactor to pre-allocate the packet memory
-		mp = append(mp, macBytes...)
+	for idx := 6; idx < packetLen; idx += len(macBytes) {
+		// Does not check slice capacity
+		copy(mp[idx:], macBytes)
 	}
 	return mp
 }
